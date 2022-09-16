9b008cd3008af063d798166588321bb3ce395b9b
Patrick Buitre <patrickbuitre@Prosources-MacBook-Pro.local>
2022-09-16 13:28:37 +0800

    First commit

---
+++ src/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CartApi';
+export * from './DefaultApi';
+export * from './ItemApi';
+export * from './StoreApi';
---
+++ src/models/index.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CartDTO';
+export * from './CartDTOCustomer';
+export * from './CartEntity';
+export * from './CartItemDTO';
+export * from './CategoryClass';
+export * from './CustomerDetailDTO';
+export * from './ExtraGroupClass';
+export * from './GroupSelectionClass';
+export * from './ItemClass';
+export * from './ItemInventoryEntity';
+export * from './ItemPartialClass';
+export * from './ItemTimeSlotClass';
+export * from './ItemsEntity';
+export * from './MenuClass';
+export * from './MenuClassItem';
+export * from './StoreClass';
+export * from './StoreHoursClass';
+export * from './StorePartialClass';
+export * from './StoresEntity';
